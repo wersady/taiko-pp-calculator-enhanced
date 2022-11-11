63407c64a4a7d2d9008991819d524ff80b86d8f9
wersady <39130839+wersady@users.noreply.github.com>
2022-11-11 11:27:59 -0500

    Added comments

--- api.js
+++ api.js
@@ -1,3 +1,8 @@
+/* 
+You need to get an osu! API key in order to use the beatmap search function.
+You can get an API key at https://osu.ppy.sh/p/api
+Place the API key inside the empty double quotes.
+*/
 const apiKeys = {
-    osu: ""
+    osu: "" //Put your osu! API key here.
 }
